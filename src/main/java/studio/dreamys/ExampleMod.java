1baf784026edc7e91948677b2d501d333a1293db
IRennie74 <116113442+Rennie07@users.noreply.github.com>
2025-04-23 23:02:55 -0400

    Changed mod info

--- src/main/java/studio/dreamys/ExampleMod.java
+++ src/main/java/studio/dreamys/ExampleMod.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "hyauto";
     public static final String VERSION = "1.0";
     
     @Mod.EventHandler
